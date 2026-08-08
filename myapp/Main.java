3a9de1c932f0c03e62debd44147f1b459385af90
ruchi <ruchirahi59@gmail.com>
2026-08-08 13:38:06 +0530

    added a new message in Main program

--- myapp/Main.java
+++ myapp/Main.java
@@ -1,5 +1,6 @@
 public class Main {
  public static void main(String[] args) {
   System.out.println("Hello I am in main function of java");
+  System.out.println("Will my workflow run automatically on push");
  }
 }
